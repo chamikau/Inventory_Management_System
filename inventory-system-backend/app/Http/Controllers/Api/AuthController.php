4e3739975094dbb7a435e2a5030de5529db48ef2
Chamika Umayanga <chamikaumayanga4@gmail.com>
2026-04-27 21:05:14 +0530

    frontend add

---
+++ inventory-system-backend/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    
+public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'user' => $user
+        ]);
+    }
+}
