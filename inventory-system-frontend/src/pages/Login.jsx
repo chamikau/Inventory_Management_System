f4774787595c6f69578be271a0efd14251dd957d
Chamika Umayanga <chamikaumayanga4@gmail.com>
2026-04-27 21:32:43 +0530

    addd frontend changes

---
+++ inventory-system-frontend/src/pages/Login.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import api from "../services/api";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      const res = await api.post("/login", {
+        email,
+        password,
+      });
+
+      localStorage.setItem("token", res.data.token);
+      window.location.href = "/dashboard";
+
+    } catch (err) {
+      setError(err.response?.data?.message || "Login failed");
+    }
+  };
+
+  return (
+    <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <form onSubmit={handleLogin} style={{ width: 300 }}>
+        <h2>Login</h2>
+
+        {error && <p style={{ color: "red" }}>{error}</p>}
+
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        /><br /><br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        /><br /><br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
